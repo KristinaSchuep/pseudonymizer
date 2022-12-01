e9d0aeaae495eef6fbaa1171b473d6566e18a121
Flavia Hug <hug@kof.ethz.ch>
2022-12-01 13:01:51 +0100

    keytable folder created and function wokr

--- R/append_keytable.R
+++ R/append_keytable.R
@@ -4,6 +4,27 @@
 # - check for inconsistencies --> warning
 # - keep only unique values
 
-append_keytable <- function(){
+id<- "pseudo_id"
+sensitive <- c("ahvnr","firstname","surname","birthday")
+append_keytable <- function(df,id,sensitive){
+  # Define which variables to extract 
+  keep_columns <- c(id,sensitive)
   
+  # Create temporary keytable
+  keytable_temp <- df[,keep_columns]
+  
+  # Check if there is already a keytable in workspace otherwise function is finished
+  
+  
+  
+  # Merge existing keytable with temporary keytable
+  
+  # Export keytable to workspace
+  now <- Sys.time()
+  name<-paste0("keytable",format(now, "_%Y%m%d_%H%M%S"), ".csv")
+  write.csv(keytable,paste0("\\keytable\\",name), row.names = FALSE)
+  
+  
+  return(df)
 }
+
