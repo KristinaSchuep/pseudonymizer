40eb882588fc95d1ed8bb6d3b3b1ed8174b9057e
Flavia Hug <hug@kof.ethz.ch>
2022-12-01 12:00:11 +0100

    empty script for append keytable function

---
+++ R/append_keytable.R
@@ -0,0 +1,9 @@
+# Task 3: Generate and append key table ---------------------------
+# - generate new key table with unique ID, AHV number, name, address, birth date
+# - load last key table, append 
+# - check for inconsistencies --> warning
+# - keep only unique values
+
+append_keytable <- function(){
+  
+}
