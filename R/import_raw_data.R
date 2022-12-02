46e06eae9b9283dd056d54645700e12871f018e3
Flavia Hug <hug@kof.ethz.ch>
2022-12-02 11:05:11 +0100

    condenced import function by taking predefined variables to replace column names

--- R/import_raw_data.R
+++ R/import_raw_data.R
@@ -1,21 +1,21 @@
 # Task 0: Import datasets (mostly as excel files) into R -----------------------
 
-
 import_raw_data <- function(filename,
                             sheetname =NULL,
-                            skiprows = 0,
-                            ahvnr,
-                            firstname,
-                            surname,
-                            birthday){
+                            skiprows = 0){
   
   df <- readxl::read_excel(filename,sheet=sheetname,skip=skiprows)
-  colnames(df)[colnames(df) == ahvnr] <- "ahvnr"
-  colnames(df)[colnames(df) == firstname] <- "firstname"
-  colnames(df)[colnames(df) == surname] <- "surname"
-  colnames(df)[colnames(df) == birthday] <- "birthday"
+  
+  index <- match(oldnames, names(df))
+  colnames(df)[index] <- newnames
   names(df) <- tolower(names(df))
   return(as.data.frame(df))
+  
+  # Check that oldnames are df column names
+  assertthat::assert_that(all(oldnames %in% names(df)), msg = "One or more column name cannot be replaced, because it does not exist. Verify that all names in 'oldnames' are actually in dataframe")
+  
+  #Check that oldnames and newnames are same length
+  assertthat::assert_that(length(oldnames)==length(newnames), msg = "The number of column names in 'oldnames' does not match the number of replacing column names in 'newnames'")
 }
 
 
