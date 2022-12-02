42dfebd858e7b32bf611743cfee9d27764dc3ece
Flavia Hug <hug@kof.ethz.ch>
2022-12-02 12:15:49 +0100

    check whether subfolder keyfolder already exists otherwise create

--- R/append_keytable.R
+++ R/append_keytable.R
@@ -3,28 +3,35 @@
 # - load last key table, append 
 # - check for inconsistencies --> warning
 # - keep only unique values
+main_dir <- getwd()
+sub_dir <- file.path("data","keytable")
 
-id<- "pseudo_id"
+pseudo_id<- "pseudo_id"
 sensitive <- c("ahvnr","firstname","surname","birthday")
-append_keytable <- function(df,id,sensitive){
+
+append_keytable <- function(df){
+  # Check if ./data/keytable directory already exists otherwise create
+  dir.create(file.path(main_dir, sub_dir), showWarnings = FALSE)
+  
   # Define which variables to extract 
-  keep_columns <- c(id,sensitive)
+  keep_columns <- c(pseudo_id,sensitive)
   
   # Create temporary keytable
-  keytable_temp <- df[,keep_columns]
+  keytable_temp <- df_pop[,keep_columns]
   
   # Check if there is already a keytable in workspace otherwise function is finished
-  
-  
+  list.files(file.path(main_dir, sub_dir), pattern = "keytable")
   
   # Merge existing keytable with temporary keytable
   
-  # Export keytable to workspace
+  # Export keytable to ./data/keytable folder 
   now <- Sys.time()
+  path <- getwd()
   name<-paste0("keytable",format(now, "_%Y%m%d_%H%M%S"), ".csv")
-  write.csv(keytable,paste0("\\keytable\\",name), row.names = FALSE)
+
+  write.csv(keytable,file.path(path,"data","keytable",name), row.names = FALSE)
   
-  
+
   return(df)
 }
 
