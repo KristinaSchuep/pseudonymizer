3348e101f7cf03b72d3bda0d5be7a4a7f32668cf
Kristina Schüpbach <62543287+KristinaSchuep@users.noreply.github.com>
2022-12-07 18:05:17 +0100

    Format date variables

--- R/aggregate_sensitive.R
+++ R/aggregate_sensitive.R
@@ -4,6 +4,7 @@
 #'
 #' @param data Dataframe
 #' @param date_var Date variable
+#' @param date_old_format Format of old date variable
 #' @param date_new_format Format for new date variable
 #' @param date_new_var Name for new date variable
 #'
@@ -11,12 +12,12 @@
 #' @export
 aggregate_sensitive <- function(data,
                                 date_var = "birthday",
+                                date_old_format = "%d.%m.%Y",
                                 date_new_format = "%Y",
                                 date_new_var = "birthyear"){
-  date_var_ <- data[, date_var]
-  date_var_ <- as.Date(date_var_)
-  date_var_ <- as.numeric(format(date_var_, format = date_new_format))
-  data[, date_new_var] <- date_var_
+  data[, date_var] <- as.Date(data[, date_var], date_old_format, tz = "UTC")
+  data[, date_new_var] <- as.numeric(format(data[, date_var], format = date_new_format))
   return(data)
 }
+
 
