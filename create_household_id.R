e195a53765612a3e5f39514a3e71d3e8af536b61
Flavia Hug <hug@kof.ethz.ch>
2022-12-23 09:01:01 +0100

    create household id approximation

--- create_household_id.R
+++ create_household_id.R
@@ -14,17 +14,38 @@ create_household_id <- function(data,
                                 var_street = "strasse",
                                 var_number = "hausnr",
                                 var_surname = "surname"){
+  # Check if Familysize=1
+  
   # Create groups of those with same adresse (Street + Housenumber)
   address<-paste(data[,var_street],data[,var_number])
   data$address <- gsub(" ", "",gsub("[[:punct:]]", "", address))
-  data$dossier <- with(data, match(address, sort(unique(address))))
   
-  # Check whether someone has the same last name
-  # If so put them into the same group
-  # If possible account for some using double names and some not. see example FAKE_2019 Family Ackermann
+  #assign Geo_id based on address
+  #data$geo_id <- cumsum(!duplicated(data[,c("address")]))
+  
+  # Check if Entries per Adresse=1
+  
+  # Count number ob "Antagsteller" per geo_id
+  antragssteller_per_geo_id  <- aggregate(rep(1, length(paste0(data$geo_id, data$rolle))), by=list(data$geo_id, data$rolle), sum)
+  temp <- do.call(data.frame, antragssteller_per_geo_id)
+  colnames(temp) <- c("geo_id","rolle", "poss_applicants")
+  temp <- temp[temp$rolle=="Antragssteller",]
+  data <- (merge(data, temp[,c("geo_id","poss_applicants")], by = "geo_id"))
+  
+  # Check if within one address poss_applicants = 1
+  
+  # Check if within one address entry per address=familysize
+  
+  # Check if within one address and one ewid, entries=familysize
+  
+  # Check if within one address and one surname, entries=familysize
+  # If possible account for double names 
+  
+  # Else NA 
+  
+
 
   return(data)
-}
 }
 
 
