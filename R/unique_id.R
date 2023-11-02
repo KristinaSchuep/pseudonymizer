a4b00dd158e35f90ddceb5459c16f86ef56f357c
Gibberish1723 <55234711+Gibberish1723@users.noreply.github.com>
2023-11-02 16:35:50 +0100

    take pseudo_id as input variable

--- R/unique_id.R
+++ R/unique_id.R
@@ -4,6 +4,7 @@
 #'
 #' @param data Dataframe
 #' @param id ID-variable that needs to by pseudonymized
+#' @param pseudo_id Pseudonymized ID-variable
 #' @param salt Salt value
 #' @param id_expected_format Expected format of ID-variable (Default: 'digit').
 #' If the ID-variable does not have the expected format, the function will try to
@@ -16,25 +17,26 @@
 #' @export
 unique_id <- function(data,
                       id = "ahvnr",
+                      pseudo_id,
                       salt,
                       id_expected_format = "digit",
                       id_expected_length = 13){
 
   # ID to character (necessary for hash function)
-  data[,id] <- as.character(data[, id])
-  ids <- data[,id]
-  
+  data[, id] <- as.character(data[, id])
+  ids <- data[, id]
+
   # Generate a variable with the format of the id
   # (this allows the researcher who receives the final pseudonymized data
   # to check whether there were any anomalies in the id)
-  data[, "id_format"] <- gsub('(?<=.{3})[[:digit:]]', '9', ids, perl = TRUE)
+  data[, "id_format"] <- gsub("(?<=.{3})[[:digit:]]", "9", ids, perl = TRUE)
 
-  # Remove all strings that are not numeric or characters 
-  # AHV Number should be all numeric, 
+  # Remove all strings that are not numeric or characters
+  # AHV Number should be all numeric,
   # by keeping the characters, people with systematically deviating ahv numbers can be distinguished from each other
-  
+
   ids <- gsub(pattern = "[^[:alnum:]]", replacement = "", ids)
-    
+
   # Save this cleaned ahv-number
   data[, id] <- ids
 
@@ -43,10 +45,10 @@ unique_id <- function(data,
   # assertthat::assert_that(all(grepl(expect, ids)|is.na(ids)), msg = "The id-variable does not have the expected format and cannot be transformed automatically. Please transform the id-variable manually or change the expected format")
 
   # Pseudonymize id-variable
-  data[, "pseudo_id"] <- openssl::sha256(x = ids, key = salt)
+  data[, pseudo_id] <- openssl::sha256(x = ids, key = salt)
 
   #Save as character vector
-  class(data$pseudo_id) <- "character"
+  class(data[, pseudo_id]) <- "character"
 
   return(data)
 }
