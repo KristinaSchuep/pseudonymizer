1b3b02ddc1af1faf4aae7cf44c6ad8bf801580f5
Gibberish1723 <55234711+Gibberish1723@users.noreply.github.com>
2023-11-02 16:53:27 +0100

    make flexible for secondary and primary

--- R/append_keytable.R
+++ R/append_keytable.R
@@ -4,22 +4,24 @@
 #' @param path Filepath
 #' @param keytable_vars Variables to keep in keytable
 #' @param id ID-variable that needs to by pseudonymized
-#'
+#' @param pseudo_id Pseudonymized ID-variable
+
 #' @return Writes or appends keytable
 #' @export
 append_keytable <- function(df,
                             path,
                             keytable_vars,
-                            id){
+                            id,
+                            pseudo_id) {
   message(paste0("------- Append Key Table -------"))
   # Check if keytable directory already exists otherwise create
   dir.create(file.path(path, "keytable"), showWarnings = FALSE)
 
   # Define which variables to extract
-  keep_columns <- c("pseudo_id",keytable_vars)
+  keep_columns <- c(pseudo_id, keytable_vars)
 
   # Create temporary keytable
-  keytable_temp <- df[,keep_columns]
+  keytable_temp <- df[, keep_columns]
 
   # Add system time as variable
   now <- Sys.time()
@@ -27,18 +29,18 @@ append_keytable <- function(df,
 
   # List of all "*keytable.csv" files in keytable subfolder
   # Load newest keytable with temporary keytable
-  newest <- sort(list.files(file.path(path, "keytable"), pattern = "keytable.csv"),decreasing = TRUE)[1]
-  keytable_temp <- as.data.frame(sapply(keytable_temp[,1:length(keytable_temp)],as.character))
+  newest <- sort(list.files(file.path(path, "keytable"), pattern = "keytable.csv"), decreasing = TRUE)[1]
+  keytable_temp <- as.data.frame(sapply(keytable_temp[, 1:length(keytable_temp)], as.character))
 
   col_classes <- c(rep("character", length(keytable_temp)))
 
     if(is.na(newest)) {
     # If there is no keytable
     # Export current keytable to ./keytable folder
-    keytable_temp <- keytable_temp[order(keytable_temp$created, keytable_temp[,id], decreasing = TRUE), ]
+    keytable_temp <- keytable_temp[order(keytable_temp$created, keytable_temp[, id], decreasing = TRUE), ]
     n <- nrow(keytable_temp)
 
-    keytable_temp <- keytable_temp[!duplicated(keytable_temp[c(id,"birthday")]), ]
+    keytable_temp <- keytable_temp[!duplicated(keytable_temp[c(id, "birthday")]), ]
     message(paste0(n - nrow(keytable_temp), " duplicates based on AHV-number and birthday removed. Keytable now containts ", nrow(keytable_temp), " observations with ",nrow(keytable_temp[!duplicated(keytable_temp[c(id)]),]), " unique AHV-numbers."))
 
     name <- paste0(format(now, "%Y%m%d_%H%M%S"), "_keytable",".csv")
